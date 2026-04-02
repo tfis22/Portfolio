778283745fdd242d6dbdd21c72215120d09d5299
Bruce Klenam <brucedumashie@gmail>
2026-04-02 05:12:29 +0100

    loading screen and navbar added

---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+
+export const Navbar = ({menuOpen, setMenuOpen}) =>{
+
+    useEffect(() =>{
+        document.body.style.overflow = menuOpen ?"hidden" :"";
+    }, [menuOpen]);
+
+    return <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <div className="max-w-5xl mx-auto px-4 ">
+            <div className="flex justify-between items items-center h-16">
+                <a href="#home" className="font-mono text-xl font-bold text-white">tf!<span className="text-blue-500">22</span></a>
+                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() =>setMenuOpen((prev)=>!prev)}>
+                    &#9776;
+                </div>
+
+                <div className="hidden md:flex items-center space-x-8">
+                    <a href="#Home" className="text-gray-300 hover:text-white transition-colors">{" "}Home{" "}</a>
+                    <a href="#About" className="text-gray-300 hover:text-white transition-colors">{" "}About{" "}</a>
+                    <a href="#Projects" className="text-gray-300 hover:text-white transition-colors">{" "}Projects{" "}</a>
+                    <a href="#Contacts" className="text-gray-300 hover:text-white transition-colors">{" "}Contacts{" "}</a>
+
+                </div>
+
+            </div>
+        </div>
+    </nav>
+}
